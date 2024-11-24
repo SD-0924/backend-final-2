eb7336f4e1325f5697f40ad71b68480b843067a5
Loai <43700105+Loai106@users.noreply.github.com>
2024-11-24 18:20:12 +0300

    rewrite verify token middleware

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,120 +1,109 @@
-import { Request, Response, NextFunction } from "express";
-import UserService from "../services/userServices";
-import generateToken from "../utils/generateToken";
-import { configDotenv } from "dotenv";
+import { Request, Response, NextFunction } from 'express'
+import UserService from '../services/userServices'
+import generateToken from '../utils/generateToken'
+import { configDotenv } from 'dotenv'
 import {
   validateUserSignUp,
   validateMerchantSignUp,
-} from "../utils/validateUser";
-import MerchantService from "../services/merchantService";
+} from '../utils/validateUser'
+import MerchantService from '../services/merchantService'
+import bcrypt from 'bcrypt'
 
-configDotenv();
+configDotenv()
 export const signUp = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    let result: any; //mercahnt or user object
+    let result: any //mercahnt or user object
 
-    if (req.body.role === "user") {
+    if (req.body.role === 'user') {
       //validation logic
 
-      const { value, error } = validateUserSignUp(req.body);
+      const { value, error } = validateUserSignUp(req.body)
 
       if (error) {
-        res.status(400).send(error + "");
-        return;
+        res.status(400).send(error + '')
+        return
       }
 
       //make sure user does not exist
       if (await UserService.findUserByEmail(value.email)) {
         res.status(400).json({
           success: false,
-          message: "User already exists, please login.",
-        });
-        return;
+          message: 'User already exists, please login.',
+        })
+        return
       }
       //create user
-      const user = await UserService.createUser(value);
+      const user = await UserService.createUser(value)
       if (!user) {
-        throw new Error("Internal server Error");
+        throw new Error('Internal server Error')
       }
-      result = user;
+      result = user
     }
 
-    if (req.body.role === "merchant") {
+    if (req.body.role === 'merchant') {
       //validation logic
 
-      const { value, error } = validateMerchantSignUp(req.body);
+      const { value, error } = validateMerchantSignUp(req.body)
       if (error) {
-        res.status(400).send(error + "");
-        return;
+        res.status(400).send(error + '')
+        return
       }
       //make sure merchant does not exist
       if (await MerchantService.findMerchantByEmail(value.email)) {
         res.status(400).json({
           success: false,
-          message: "User already exists, please login.",
-        });
-        return;
+          message: 'User already exists, please login.',
+        })
+        return
       }
-      const merchant = await MerchantService.createUser(value);
+      const merchant = await MerchantService.createUser(value)
       if (!merchant) {
-        throw new Error("Internal server Error");
+        throw new Error('Internal server Error')
       }
-      result = merchant;
+      result = merchant
     }
     //generate token
     const token = generateToken({
       id: result.user_id || result.merchant_id,
       role: req.body.role,
-    });
+    })
     //send back to the user
     res.status(201).json({
       success: true,
       token,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-import {Request, Response, NextFunction } from "express";
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password, role } = req.body()
 
+  const user = await UserService.findUserByEmail(email)
 
-
-export const login = async(req : Request, res : Response,next: NextFunction)=>{
-
-    const {email,password,role} =req.body();
-    
-    const user = User.findOne({
-        where:{
-            email,
-        }
-    })    
-
-    if(!user){
-        res.status(404).send('your email or password ')
-    }
-    else{
-        const payload ={
-            id: user.id
-        }
-
-        //check if the password is correct 
-        if(await bcrypt.compare(password,user.password)){
-            const token = generateToken(payload,role);
-            res.status(200).json({token});
-        }
-        else{
-            res.status(404).json({
-                message:"your email or password is wrong"
-            })
-        }
+  if (!user) {
+    res.status(404).send('your email or password ')
+  } else {
+    const payload = {
+      id: user.dataValues.id,
+      role,
     }
 
-
-
-
+    //check if the password is correct
+    if (await bcrypt.compare(password, user.dataValues.password)) {
+      const token = generateToken(payload)
+      res.status(200).json({ token })
+    } else {
+      throw new Error('your email or password is wrong');
+    }
+  }
 }
